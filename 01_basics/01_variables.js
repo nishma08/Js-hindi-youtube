e09749740a7b585c5cf0def6516c7122dfd1b833
nishma08 <164840804+nishma08@users.noreply.github.com>
2025-01-10 11:35:48 +0000

    02 done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,9 +1,19 @@
 const accountId = 144553
-let accountEmail = "noish@gmail.com"
+let accountEmail = "nish@gmail.com"
 var accountPassword = "123545"
 
 accountCity = "Sydney"
 
 //accountId = 2 //not allowed
 
-console.log(accountId);
+accountEmail = "hc.vn"
+accountPassword = "232323"
+accountCity = "melbourne"
+let accountState;
+
+console.log(accountId); 
+/* Prefer not to use var 
+because of iussue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // this allows systewm to treat all the js code as newer version
+
+// alert (3+3) // We are using nodejs, not the browser
+
+console.log(3+3) // code readability should be high
+
+let name = "nishma"
+let age = 20
+let isLoggedIn = false
+
+// number => to power 53
+//bigint for large numbers
+//string => ""
+//boolean => true/false
+//null => stand alone value
+//undefined => not yet declared
+//symbol => uniqueness
+
+//object 
+
+console.log(typeof undefined )
+
