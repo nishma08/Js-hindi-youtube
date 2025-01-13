54d4dd4871caa79c61e75d70bf6d8335f254adbb
nishma08 <164840804+nishma08@users.noreply.github.com>
2025-01-13 11:29:37 +0000

    03_Done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = undefined
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+//const {score} = req.body
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33" => 33
+// "33abc" => NaN
+//"null" => 0
+// "undefined" => NaN
+// "true" => 1 ; false => 0
+
+//let isLoggedIn = "nishma"
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "nishma" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+console.log ("hello world")
+//every files needs to be commited and pushed
