907179db1e97ab5591af44284bdb2d07e2247ba2
nishma08 <164840804+nishma08@users.noreply.github.com>
2025-01-16 07:54:11 +0000

    conversio and operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,9 +24,42 @@ let score = undefined
 // "" => false
 // "nishma" => true
 
-let someNumber = 33
+/*let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
 console.log ("hello world")
-//every files needs to be commited and pushed
+//every files needs to be commited and pushed */
+
+// ** Operations ** //
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+/*
+let str1 = "hello"
+let str2 = " nishma"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log( "1" + "2" );
+console.log( 1 + 2 + "2" );
+
+console.log(+true);
+console.log(+ "");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2; */
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
