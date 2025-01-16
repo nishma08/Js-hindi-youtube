71748f829851f0c03c4cc8be91267c39296a6e52
nishma08 <164840804+nishma08@users.noreply.github.com>
2025-01-16 08:22:27 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//console.log(2 != 1);
+//console.log(2 == 1);
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+/*
+{
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+} // This kind of comparision should be avoided as it can cause problem
+*/
+
+// === 
+
+console.log("2" === 2);
+
+
+
+
